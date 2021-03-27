6e8653abb1c5d41bd32aeb546b789f931904fd3f
JhormaMalave <jhormamalave99@gmail.com>
2021-03-26 22:08:57 -0400

    Generate product model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :products, dependent: :destroy
+  has_secure_password
+
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
 
-  has_secure_password
 end
