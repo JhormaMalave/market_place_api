0790ff4da2d013804994f4c52f1a2526bcea1b37
JhormaMalave <jhormamalave99@gmail.com>
2021-03-27 21:20:44 -0400

    Create a seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.delete_all
+Product.delete_all
+
+3.times do
+  user = User.create! email: Faker::Internet.email, password: 'locadex1234'
+  puts "Created a new user: #{user.email}"
+
+  2.times do
+    product = Product.create!(
+      title: Faker::Commerce.product_name,
+      price: rand(1.0..10.0),
+      published: true,
+      user_id: user.id
+    )
+    puts "Created a new product: #{product.title}"
+  end
+end
