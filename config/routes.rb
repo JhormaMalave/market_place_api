62082cd4a61e90b2bf2ec91011ed2d398c8212ec
JhormaMalave <jhormamalave99@gmail.com>
2021-04-04 17:01:15 -0400

    Adds the index action for order

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
+      resources :orders, only: [:index]
       resources :products, only: %i[index show create update destroy]
     end
   end
